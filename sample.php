3ed6522f9a7dbb3bd1444bd8ba6d7bea265eacf6
sibazi <yuzu.taichi.shiba@gmail.com>
2023-11-25 23:13:20 +0900

    first commit

---
+++ sample.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>PHPテスト</title>
+</head>
+<body>
+
+<p>こんにちは。</p>
+
+<p>
+<?php
+echo "お元気ですか？";
+?>
+</p>
+
+<p>
+<?php
+echo "こんにちは";
+echo "こんにちは";
+echo "こんにちは";
+?>
+</p>
+
+<p>
+<?php
+echo "こんにちは";echo "こんにちは";echo "こんにちは";
+?>
+</p>
+
+<p>
+<?php
+echo "こんにちは";
+echo "こんにちは";
+echo "こんにちは"
+?>
+</p>
+
+<p>
+<?php
+echo "こんにちは。<br>";
+  echo "こんにちは。<br>";
+echo   "こんにちは。<br>";
+echo
+"こんにちは。<br>";
+?>
+</p>
+
+</body>
+</html>
